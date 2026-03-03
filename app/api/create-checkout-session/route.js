0ccbfc8119de68ceb1f6dc1985d7bef2656fab77
Muhammad Atif khan <22pwbcs0906@uetpeshawar.edu.pk>
2026-03-03 17:13:18 +0500

    problem fixxx

--- app/api/create-checkout-session/route.js
+++ app/api/create-checkout-session/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { getStripe } from "../../../lib/scalekit"; // relative path
+import { getStripe } from "../../../lib/stripe"; // relative path
 
 export async function POST(req) {
   try {
